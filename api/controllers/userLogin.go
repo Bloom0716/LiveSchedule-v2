26233eefbde187be4fb412988098041e299740f2
Rintaro Takata <rinta8994@gmail.com>
2024-02-03 01:45:05 +0900

    Create Login function

---
+++ api/controllers/userLogin.go
@@ -0,0 +1,72 @@
+package controllers
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/LiveSchedule-v2/initializers"
+	"github.com/LiveSchedule-v2/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Login(c *gin.Context) {
+	// Get the req
+	var body struct {
+		Email    string `form:"email"`
+		Password string `form:"password"`
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	// Look up requested user
+	user := models.User{}
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email",
+		})
+		return
+	}
+
+	// Compare password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid password",
+		})
+		return
+	}
+
+	// Generate a jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid to create token",
+		})
+		return
+	}
+
+	// Respond
+	c.JSON(http.StatusOK, gin.H{
+		"email":    body.Email,
+		"password": body.Password,
+		"token":    tokenString,
+	})
+}
--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/LiveSchedule-v2/controllers"
 	"github.com/LiveSchedule-v2/initializers"
 	"github.com/gin-gonic/gin"
 )
@@ -12,11 +13,9 @@ func init() {
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	router := gin.Default()
+	router.POST("/signup", controllers.Signup)
+	router.POST("/login", controllers.Login)
+
+	router.Run()
 }
