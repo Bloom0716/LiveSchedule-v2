6b6f63b6a713244ed1d09f3b4ae6c53ca867b094
Rintaro Takata <rinta8994@gmail.com>
2024-02-04 22:56:54 +0900

    Add middleware

---
+++ api/middleware/requireAuth.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/LiveSchedule-v2/initializers"
+	"github.com/LiveSchedule-v2/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func RequireAuth(c *gin.Context) {
+	// Get token
+	tokenString := c.Request.Header.Get("Authorization")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		// Check the exp
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		// Find the user with token sub
+		user := models.User{}
+		initializers.DB.First(&user, claims["sub"])
+
+		if user.ID == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		c.Set("User", user)
+
+	} else {
+		fmt.Println(err)
+	}
+
+	// Continue
+	c.Next()
+}
